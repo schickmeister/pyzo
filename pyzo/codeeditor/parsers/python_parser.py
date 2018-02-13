fd45731553f6b2cbc5df9aafd7deadca04021662
Austin Schick <schick.austin@gmail.com>
2018-02-13 14:09:14 -0500

    Added functionality for highlighting builtin functions

--- pyzo/codeeditor/parsers/python_parser.py
+++ pyzo/codeeditor/parsers/python_parser.py
@@ -12,8 +12,8 @@ from ..misc import ustr
 # Import tokens in module namespace
 from .tokens import (CommentToken, StringToken,
     UnterminatedStringToken, IdentifierToken, NonIdentifierToken,
-    KeywordToken, BuiltinsToken, InstanceToken, NumberToken, FunctionNameToken, ClassNameToken,
-    TodoCommentToken, OpenParenToken, CloseParenToken)
+    KeywordToken, NumberToken, FunctionNameToken, ClassNameToken,
+    TodoCommentToken, OpenParenToken, CloseParenToken, BuiltinToken)
 
 # Keywords sets
 
@@ -28,96 +28,47 @@ python3Keywords = set(['False', 'None', 'True', 'and', 'as', 'assert', 'break',
         'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally',
         'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
         'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while',
-        'with', 'yield', 'async', 'await'])
+        'with', 'yield'])
+
+# Builtin sets
+
+#Source: https://docs.python.org/2/library/functions.html
+python2Builtins = set([ 'abs', 'all', 'any', 'basestring', 'bin', 'bool',
+        'bytearray', 'callable', 'chr', 'classmethod', 'cmp', 'compile',
+        'complex', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval',
+        'execfile', 'file', 'filter', 'float', 'format', 'frozenset', 'getattr',
+        'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int',
+        'isinstance', 'issubclass', 'iter', 'len', 'list', 'locals', 'long',
+        'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open',
+        'ord', 'pow', 'print', 'property', 'range', 'raw_input', 'reduce',
+        'reload', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice',
+        'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple', 'type',
+        'unichr', 'unicode', 'vars', 'zip', '__import__'])
+
+#Source: https://docs.python.org/3.6/library/functions.html
+python3Builtins = set([ 'abs', 'all', 'any', 'ascii', 'bin', 'bool',
+        'bytearray', 'bytes', 'callable', 'chr',
+        'classmethod', 'compile', 'complex', 'delattr', 'dict', 'dir', 'divmod',
+        'enumerate', 'eval', 'exec', 'filter', 'float', 'format', 'frozenset',
+        'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input',
+        'int', 'isinstance', 'issubclass', 'iter', 'len', 'list', 'locals',
+        'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open',
+        'ord', 'pow', 'print', 'property', 'range', 'repr', 'reversed', 'round',
+        'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum',
+        'super', 'tuple', 'type', 'vars', 'zip', '__import__'])
 
 # Merge the two sets to get a general Python keyword list
 pythonKeywords = python2Keywords | python3Keywords
-
-# Builtins sets
-
-# Source: dir (__builtins__) (Python 2.7.12)
-python2Builtins = set(['ArithmeticError', 'AssertionError', 'AttributeError',
-        'BaseException', 'BufferError', 'BytesWarning', 'DeprecationWarning',
-        'EOFError', 'Ellipsis', 'EnvironmentError', 'Exception', 'False',
-        'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError',
-        'ImportError', 'ImportWarning', 'IndentationError', 'IndexError',
-        'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError',
-        'NameError', 'None', 'NotImplemented', 'NotImplementedError',
-        'OSError', 'OverflowError', 'PendingDeprecationWarning',
-        'ReferenceError', 'RuntimeError', 'RuntimeWarning', 'StandardError',
-        'StopIteration', 'SyntaxError', 'SyntaxWarning', 'SystemError',
-        'SystemExit', 'TabError', 'True', 'TypeError', 'UnboundLocalError',
-        'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError',
-        'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError',
-        'Warning', 'ZeroDivisionError', '__debug__', '__doc__', '__import__',
-        '__name__', '__package__', 'abs', 'all', 'any', 'apply', 'basestring',
-        'bin', 'bool', 'buffer', 'bytearray', 'bytes', 'callable', 'chr',
-        'classmethod', 'cmp', 'coerce', 'compile', 'complex', 'copyright',
-        'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval',
-        'execfile', 'exit', 'file', 'filter', 'float', 'format', 'frozenset',
-        'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input',
-        'int', 'intern', 'isinstance', 'issubclass', 'iter', 'len', 'license',
-        'list', 'locals', 'long', 'map', 'max', 'memoryview', 'min', 'next',
-        'object', 'oct', 'open', 'ord', 'pow', 'print', 'property', 'quit',
-        'range', 'raw_input', 'reduce', 'reload', 'repr', 'reversed', 'round',
-        'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum',
-        'super', 'tuple', 'type', 'unichr', 'unicode', 'vars', 'xrange',
-        'zip'])
-
-# Source: import builtins; dir(builtins) (Python 3.5.2)
-# Note: Removed 'False', 'None', 'True'. They are keyword in Python 3
-python3Builtins = set(['ArithmeticError', 'AssertionError', 'AttributeError',
-        'BaseException', 'BlockingIOError', 'BrokenPipeError', 'BufferError',
-        'BytesWarning', 'ChildProcessError', 'ConnectionAbortedError',
-        'ConnectionError', 'ConnectionRefusedError', 'ConnectionResetError',
-        'DeprecationWarning', 'EOFError', 'Ellipsis', 'EnvironmentError',
-        'Exception', 'FileExistsError', 'FileNotFoundError',
-        'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError',
-        'ImportError', 'ImportWarning', 'IndentationError', 'IndexError',
-        'InterruptedError', 'IsADirectoryError', 'KeyError',
-        'KeyboardInterrupt', 'LookupError', 'MemoryError', 'NameError',
-        'NotADirectoryError', 'NotImplemented', 'NotImplementedError',
-        'OSError', 'OverflowError', 'PendingDeprecationWarning',
-        'PermissionError', 'ProcessLookupError', 'RecursionError',
-        'ReferenceError', 'ResourceWarning', 'RuntimeError', 'RuntimeWarning',
-        'StopAsyncIteration', 'StopIteration', 'SyntaxError', 'SyntaxWarning',
-        'SystemError', 'SystemExit', 'TabError', 'TimeoutError',
-        'TypeError', 'UnboundLocalError', 'UnicodeDecodeError',
-        'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError',
-        'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning',
-        'ZeroDivisionError', '__build_class__', '__debug__', '__doc__',
-        '__import__', '__loader__', '__name__', '__package__', '__spec__',
-        'abs', 'all', 'any', 'ascii', 'bin', 'bool', 'bytearray', 'bytes',
-        'callable', 'chr', 'classmethod', 'compile', 'complex', 'copyright',
-        'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval',
-        'exec', 'exit', 'filter', 'float', 'format', 'frozenset', 'getattr',
-        'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int',
-        'isinstance', 'issubclass', 'iter', 'len', 'license', 'list', 'locals',
-        'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open',
-        'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr', 'reversed',
-        'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str',
-        'sum', 'super', 'tuple', 'type', 'vars', 'zip'])
-
-
-# Merge the two sets to get a general Python builtins list
 pythonBuiltins = python2Builtins | python3Builtins
-
-# Instance sets
-python2Instance = set(['self'])
-
-python3Instance = set(['self'])
-
-pythonInstance = python2Instance | python3Instance
-
 
 class MultilineStringToken(StringToken):
     """ Characters representing a multi-line string. """
     defaultStyle = 'fore:#7F0000'
 
-
 class CellCommentToken(CommentToken):
     """ Characters representing a cell separator comment: "##". """
     defaultStyle = 'bold:yes, underline:yes'
+
 
 
 # This regexp is used to find special stuff, such as comments, numbers and
@@ -135,11 +86,11 @@ tokenProg = re.compile(
     )
 
 
-# For a given type of string ( ', " , ''' , """ ),get  the RegExp
-# program that matches the end. (^|[^\\]) means: start of the line
-# or something that is not \ (since \ is supposed to escape the following
-# quote) (\\\\)* means: any number of two slashes \\ since each slash will
-# escape the next one
+#For a given type of string ( ', " , ''' , """ ),get  the RegExp
+#program that matches the end. (^|[^\\]) means: start of the line
+#or something that is not \ (since \ is supposed to escape the following
+#quote) (\\\\)* means: any number of two slashes \\ since each slash will
+#escape the next one
 endProgs = {
     "'": re.compile(r"(^|[^\\])(\\\\)*'"),
     '"': re.compile(r'(^|[^\\])(\\\\)*"'),
@@ -152,19 +103,16 @@ class PythonParser(Parser):
     """ Parser for Python in general (2.x or 3.x).
     """
     _extensions = ['.py' , '.pyw']
-    # The list of keywords is overridden by the Python2/3 specific parsers
-    _keywords = pythonKeywords
-    # The list of builtins and instances is overridden by the Python2/3 specific parsers
-    _builtins = pythonBuiltins
-    _instance = pythonInstance
-    
-    
+    #The list of keywords is overridden by the Python2/3 specific parsers
+    _keywords = pythonKeywords_keywords = pythonKeywords
+    _builtin_functions = pythonBuiltins
+
     def _identifierState(self, identifier=None):
         """ Given an identifier returs the identifier state:
         3 means the current identifier can be a function.
         4 means the current identifier can be a class.
         0 otherwise.
-        
+
         This method enables storing the state during the line,
         and helps the Cython parser to reuse the Python parser's code.
         """
@@ -189,35 +137,35 @@ class PythonParser(Parser):
             state = self._idsState
             self._idsState = 0
             return state
-    
-    
+
+
     def parseLine(self, line, previousState=0):
         """ parseLine(line, previousState=0)
-        
+
         Parse a line of Python code, yielding tokens.
         previousstate is the state of the previous block, and is used
         to handle line continuation and multiline strings.
-        
+
         """
         line = text_type(line)
-        
+
         # Init
         pos = 0 # Position following the previous match
-        
+
         # identifierState and previousstate values:
         # 0: nothing special
         # 1: multiline comment single qoutes
         # 2: multiline comment double quotes
         # 3: a def keyword
         # 4: a class keyword
-        
+
         #Handle line continuation after def or class
         #identifierState is 3 or 4 if the previous identifier was 3 or 4
         if previousState == 3 or previousState == 4:
             self._identifierState({3:'def',4:'class'}[previousState])
         else:
             self._identifierState(None)
-        
+
         if previousState in [1,2]:
             token = MultilineStringToken(line, 0, 0)
             token._style = ['', "'''", '"""'][previousState]
@@ -228,11 +176,11 @@ class PythonParser(Parser):
                 if isinstance(token, BlockState):
                     return
             pos = token.end
-        
-        
+
+
         # Enter the main loop that iterates over the tokens and skips strings
         while True:
-            
+
             # Get next tokens
             tokens = self._findNextToken(line, pos)
             if not tokens:
@@ -240,33 +188,33 @@ class PythonParser(Parser):
             elif isinstance(tokens[-1], StringToken):
                 moreTokens = self._findEndOfString(line, tokens[-1])
                 tokens = tokens[:-1] + moreTokens
-            
+
             # Process tokens
             for token in tokens:
                 yield token
                 if isinstance(token, BlockState):
                     return
             pos = token.end
-    
-    
+
+
     def _findEndOfString(self, line, token):
         """ _findEndOfString(line, token)
-        
+
         Find the end of a string. Returns (token, endToken). The first
         is the given token or a replacement (UnterminatedStringToken).
         The latter is None, or the BlockState. If given, the line is
         finished.
-        
+
         """
-        
+
         # Set state
         self._identifierState(None)
-        
+
         # Find the matching end in the rest of the line
         # Do not use the start parameter of search, since ^ does not work then
         style = token._style
         endMatch = endProgs[style].search(line[token.end:])
-        
+
         if endMatch:
             # The string does end on this line
             tokenArgs = line, token.start, token.end + endMatch.end()
@@ -284,34 +232,34 @@ class PythonParser(Parser):
                 return [MultilineStringToken(*tokenArgs), BlockState(2)]
             else:
                 return [UnterminatedStringToken(*tokenArgs)]
-    
-    
+
+
     def _findNextToken(self, line, pos):
         """ _findNextToken(line, pos):
-        
+
         Returns a token or None if no new tokens can be found.
-        
+
         """
-        
+
         # Init tokens, if pos too large, were done
         if pos > len(line):
             return None
         tokens = []
-        
+
         # Find the start of the next string or comment
         match = tokenProg.search(line, pos)
-        
+
         # Process the Non-Identifier between pos and match.start()
         # or end of line
         nonIdentifierEnd = match.start() if match else len(line)
-        
+
         # Return the Non-Identifier token if non-null
         # todo: here it goes wrong (allow returning more than one token?)
         token = NonIdentifierToken(line,pos,nonIdentifierEnd)
         strippedNonIdentifier = ustr(token).strip()
         if token:
             tokens.append(token)
-        
+
         # Do checks for line continuation and identifierState
         # Is the last non-whitespace a line-continuation character?
         if strippedNonIdentifier.endswith('\\'):
@@ -326,15 +274,15 @@ class PythonParser(Parser):
             # cancel the identifierState
             if strippedNonIdentifier != '':
                 self._identifierState(None)
-        
+
         # If no match, we are done processing the line
         if not match:
             if lineContinuation:
                 tokens.append( BlockState(self._identifierState()) )
             return tokens
-        
+
         # The rest is to establish what identifier we are dealing with
-        
+
         # Comment
         if match.group() == '#':
             matchStart = match.start()
@@ -350,27 +298,26 @@ class PythonParser(Parser):
             if lineContinuation:
                 tokens.append( BlockState(self._identifierState()) )
             return tokens
-        
+
         # If there are non-whitespace characters after def or class,
         # cancel the identifierState (this time, also if there is just a \
         # since apparently it was not on the end of a line)
         if strippedNonIdentifier != '':
             self._identifierState(None)
-        
+
         # Identifier ("a word or number") Find out whether it is a key word
         if match.group(1) is not None:
             identifier = match.group(1)
             tokenArgs = line, match.start(), match.end()
-            
+
             # Set identifier state
             identifierState = self._identifierState(identifier)
-            
+
             if identifier in self._keywords:
                 tokens.append( KeywordToken(*tokenArgs) )
-            elif identifier in self._builtins and '.' + identifier not in line:
-                tokens.append(BuiltinsToken(*tokenArgs))
-            elif identifier in self._instance:
-                tokens.append(InstanceToken(*tokenArgs))
+            elif identifier in self._builtin_functions:
+                self._identifierState(None)
+                tokens.append( BuiltinToken(*tokenArgs))
             elif identifier[0] in '0123456789':
                 self._identifierState(None)
                 tokens.append( NumberToken(*tokenArgs) )
@@ -382,7 +329,7 @@ class PythonParser(Parser):
                     tokens.append( ClassNameToken(*tokenArgs) )
                 else:
                     tokens.append( IdentifierToken(*tokenArgs) )
-        
+
         elif match.group(2) is not None :
             # We have matched a string-start
             # Find the string style ( ' or " or ''' or """)
@@ -404,12 +351,10 @@ class PythonParser(Parser):
 class Python2Parser(PythonParser):
     """ Parser for Python 2.x code.
     """
-    # The application should choose whether to set the Py 2 specific parser
+     # The application should choose whether to set the Py 2 specific parser
     _extensions = []
     _keywords = python2Keywords
-    _builtins = python2Builtins
-    _instance = python2Instance
-
+    _builtin_functions = python2Builtins
 
 class Python3Parser(PythonParser):
     """ Parser for Python 3.x code.
@@ -417,5 +362,4 @@ class Python3Parser(PythonParser):
     # The application should choose whether to set the Py 3 specific parser
     _extensions = []
     _keywords = python3Keywords
-    _builtins = python3Builtins
-    _instance = python3Instance
+    _builtin_functions = python3Builtins
--- pyzo/codeeditor/parsers/tokens.py
+++ pyzo/codeeditor/parsers/tokens.py
@@ -23,15 +23,15 @@ from ..misc import ustr
 
 class Token(object):
     """ Token(line, start, end)
-    
+
     Base token class.
-    
+
     A token is a group of characters representing "something".
     What is represented, is specified by the subclass.
-    
+
     Each token class should have a docstring describing the meaning
     of the characters it is applied to.
-    
+
     """
     defaultStyle = 'fore:#000, bold:no, underline:no, italic:no'
     isToken = True # For the BlockState object, which is also returned by the parsers, this is False
@@ -40,21 +40,21 @@ class Token(object):
         self.start = start
         self.end = end
         self._name = self._getName()
-    
+
     def __str__(self):  # on 2.x we use __unicode__
         return self.line[self.start:self.end]
-    
+
     def __unicode__(self):  # for py 2.x
         return self.line[self.start:self.end]
-    
+
     def __repr__(self):
         return repr('%s:%s' % (self.name, self))
-    
+
     def __len__(self):
         # Defining a length also gives a Token a boolean value: True if there
         # are any characters (len!=0) and False if there are none
         return self.end - self.start
-    
+
     def _getName(self):
         """ Get the name of this token. """
         nameParts = ['Syntax']
@@ -64,7 +64,7 @@ class Token(object):
             nameParts.append( language[0].upper() + language[1:] )
         nameParts.append( self.__class__.__name__[:-5].lower() )
         return '.'.join(nameParts)
-    
+
     def getDefaultStyleFormat(self):
         elements = []
         def collect(cls):
@@ -77,20 +77,20 @@ class Token(object):
         for e in reversed(elements):
             se.update(e)
         return se
-    
+
     @property
     def name(self):
         """ The name of this token. Used to identify it and attach a style.
         """
         return self._name
-    
+
     @property
     def description(self):
         """ description()
-        
+
         Returns a StyleElementDescription instance that describes the
         style element that this token represents.
-        
+
         """
         format = self.getDefaultStyleFormat()
         des = 'syntax: ' + self.__doc__
@@ -131,17 +131,13 @@ class KeywordToken(IdentifierToken):
     """ A keyword is a word with a special meaning to the language. """
     defaultStyle = 'fore:#00007F, bold:yes'
 
-class BuiltinsToken(IdentifierToken):
-    """ Characters representing a builtins in the code. """
-    defaultStyle = ''
-
-class InstanceToken(IdentifierToken):
-    """ Characters representing a instance in the code. """
-    defaultStyle = ''
-
 class NumberToken(IdentifierToken):
     """ Characters represening a number. """
     defaultStyle = 'fore:#007F7F'
+
+class BuiltinToken(IdentifierToken):
+    """ Characters represening a number. """
+    defaultStyle = 'fore:#007F7F, bold:yes'
 
 class FunctionNameToken(IdentifierToken):
     """ Characters represening the name of a function. """
@@ -162,6 +158,3 @@ class OpenParenToken(ParenthesisToken) :
 class CloseParenToken(ParenthesisToken) :
     """ Closing parenthesis (and square and curly brackets). """
     defaultStyle = ''
-
-
-
